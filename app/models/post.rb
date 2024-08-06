d36b7512226482ddceac571d45d6aa2bf38b17da
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-06 15:27:55 -0400

    Complete all tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary,  length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction  Non-Fiction) }
+  validates :title, exclusion: { in: %w(True Facts) }
+
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    if title == "True Facts"
+      errors.add(:title, "No clickbait allowed")
+    end
+  end
 end
